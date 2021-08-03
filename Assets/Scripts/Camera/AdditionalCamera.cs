3cf1db4ea0a138189ade9f1ca51651c949ae8dbc
Spartanec33 <bedenya@bk.ru>
2021-08-03 10:54:39 +0300

    ВСЕ ОБОСРАНО, НО КАМЕРА ВЫПОЛЗАЕТ

--- Assets/Scripts/Camera/AdditionalCamera.cs
+++ Assets/Scripts/Camera/AdditionalCamera.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class AdditionalCamera : MonoBehaviour
 {
     [SerializeField] private Vector3 _offset;
+    [SerializeField] private GameObject _view;
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _exitPosition;
+
     private GameObject _constraction;
-    private Camera _camera;
+    private Vector3 _startPos;
+    private bool _isCoroutineActive = false;
+    private readonly WaitForFixedUpdate _waitForFixedUpdate = new WaitForFixedUpdate();
+
 
     private void Start()
     {
-        _camera = GetComponent<Camera>();
+        _startPos = _view.transform.position;
     }
 
     private void Update()
@@ -20,12 +27,32 @@ public class AdditionalCamera : MonoBehaviour
         {
             transform.position = _constraction.transform.position + _offset;
         }
-        if (Distance.Value>20)
+
+
+        if (Distance.Value < 50 && _isCoroutineActive == false)
+            StartCoroutine(Move(_view.transform.position + _exitPosition));
+        else if ((Distance.Value > 50 && _isCoroutineActive == true))
         {
-            _camera.enabled = true;
+            StartCoroutine(Move(_startPos));
+            _isCoroutineActive = false;
         }
-        else
-            _camera.enabled = false;
+
+    }
+
+    private IEnumerator Move(Vector3 endPoint)
+    {
+        _isCoroutineActive = true;
+        float allWay = Vector3.Distance(_view.transform.position, endPoint);
+        float coveredDistance = 0;
+        Vector3 viewPos = _view.transform.position;
+        while (coveredDistance <= allWay)
+        {
+            yield return _waitForFixedUpdate;
+            var progress = (coveredDistance / allWay);
+            var pos = Vector3.Lerp(viewPos, endPoint, progress);
+            _view.transform.position = pos;
+            coveredDistance += (_speed * Time.deltaTime);
+        }
     }
 
 }
