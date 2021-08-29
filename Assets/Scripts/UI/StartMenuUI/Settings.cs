2ecdbb6d9dd7e74779d60bc7c778bbc29495f0cd
Spartanec33 <bedenya@bk.ru>
2021-08-29 19:33:21 +0300

    настройки теперь сохраняются

--- Assets/Scripts/UI/StartMenuUI/Settings.cs
+++ Assets/Scripts/UI/StartMenuUI/Settings.cs
@@ -7,43 +7,124 @@ using TMPro;
 
 public class Settings : MonoBehaviour
 {
+    [Header("BackgroundMusic")]
     [SerializeField] private AudioMixerGroup _audioMixerBackGround;
+    [SerializeField] private Toggle _backgroundMusicToggle;
+
+    [Header("Resolution")] 
     [SerializeField] private TMP_Dropdown _resolutionDropdown;
-    private Resolution[] _resolutions;
+    [SerializeField] private int _resolutionsMoreThanTheCurrent;
+    [SerializeField] private int _resolutionsLessThanTheCurrent;
+    [SerializeField] private float _resolutionMultiplier = 0.9f;
+
+    [Header("Graphic")]
+    [SerializeField] private TMP_Dropdown _graphicDropdown;
+
+    private List<Resolution> _resolutions = new List<Resolution>();
+    private int _currentResolutionIndex;
+    private Resolution _mainResolution;
+
+
     private void Awake()
     {
+        if (!PlayerPrefs.HasKey("screenWidth"))
+            SaveBaseResolution();
+        _mainResolution = GetBaseResolution();
         InitializeResolutionDropdown();
+        LoadData();
     }
 
     private void InitializeResolutionDropdown()
     {
-        _resolutions = Screen.resolutions;
+        Resolution baseResolution = _mainResolution;
+        for (int i = 0; i < _resolutionsMoreThanTheCurrent+1; i++)
+        {
+            baseResolution.width = (int)(baseResolution.width / _resolutionMultiplier);
+            baseResolution.height = (int)(baseResolution.width / _resolutionMultiplier);
+        }
+        for (int i = 0; i < _resolutionsMoreThanTheCurrent; i++)
+        {
+            baseResolution = GetResolution(baseResolution);
+        }
+
+        baseResolution = _mainResolution;
+        _resolutions.Add(baseResolution);
+
+        for (int i = 0; i < _resolutionsLessThanTheCurrent; i++)
+        {
+            baseResolution = GetResolution(baseResolution);
+        }
+
         _resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < _resolutions.Length; i++)
+        for (int i = 0; i < _resolutions.Count; i++)
         {
-            string option = _resolutions[i].width + "x" + _resolutions[i].height;
-            if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
-                currentResolutionIndex = i;
+            string option = _resolutions[i].ToString();
+            if (_resolutions[i].width == _mainResolution.width && _resolutions[i].height == _mainResolution.height)
+                _currentResolutionIndex = i;
             options.Add(option);
         }
         _resolutionDropdown.AddOptions(options);
-        _resolutionDropdown.value = currentResolutionIndex;
-        _resolutionDropdown.RefreshShownValue();
+
+        Resolution GetResolution(Resolution baseResolution)
+        {
+            baseResolution.width = (int)(baseResolution.width * _resolutionMultiplier);
+            baseResolution.height = (int)(baseResolution.height * _resolutionMultiplier);
+            var resToAdd = new Resolution
+            {
+                width = baseResolution.width,
+                height = baseResolution.height,
+                refreshRate = baseResolution.refreshRate
+            };
+            _resolutions.Add(resToAdd);
+            return baseResolution;
+        }
     }
 
     public void SetResolution(int index)
     {
         Resolution resolution = _resolutions[index];
         Screen.SetResolution(resolution.width, resolution.height, true);
+        PlayerPrefs.SetInt("resolutionIndex", index);
+        PlayerPrefs.Save();
     }
     public void SetQulity(int index)
     {
         QualitySettings.SetQualityLevel(index);
+        PlayerPrefs.SetInt("graphicIndex", index);
+        PlayerPrefs.Save();
     }
-    public void SetActiveMusic(bool state)
+    public void ToggleBackgroundMusic(bool state)
     {
         _audioMixerBackGround.audioMixer.SetFloat("backGroundVolume", state ? 0 : -80);
+        PlayerPrefs.SetInt("isBackgroundMusicActive", state ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadData()
+    {
+        _resolutionDropdown.value = PlayerPrefs.GetInt("resolutionIndex", _currentResolutionIndex);
+        _resolutionDropdown.RefreshShownValue();
+
+        _graphicDropdown.value = PlayerPrefs.GetInt("graphicIndex", 3);
+        _graphicDropdown.RefreshShownValue();
+
+        _backgroundMusicToggle.isOn = PlayerPrefs.GetInt("isBackgroundMusicActive", 1) == 1 ? true : false;
+    }
+
+    private void SaveBaseResolution()
+    {
+        var baseResolution = Screen.currentResolution;
+        PlayerPrefs.SetInt("screenWidth", baseResolution.width);
+        PlayerPrefs.SetInt("screenHeight", baseResolution.height);
+    }
+    private Resolution GetBaseResolution()
+    {
+        var resolution = new Resolution
+        {
+            width = PlayerPrefs.GetInt("screenWidth"),
+            height = PlayerPrefs.GetInt("screenHeight"),
+        };
+        return resolution;
     }
 }
