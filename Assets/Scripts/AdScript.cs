10def2bebec127b7aefb1f99622cb70113577407
Spartanec33 <bedenya@bk.ru>
2021-09-15 16:57:15 +0300

    появилась возможность менять частоту рекламы

--- Assets/Scripts/AdScript.cs
+++ Assets/Scripts/AdScript.cs
@@ -6,25 +6,52 @@ namespace UseAd
 {
     public class AdScript : MonoBehaviour
     {
-        private const string AppKey = "d6518eddf62f6e0e727aa24f1677caf8f8a6ef3d377c138b";
+        [SerializeField] private int _deathToAd;
+
+        private int _deadCount; 
+        private const string APPKEY = "d6518eddf62f6e0e727aa24f1677caf8f8a6ef3d377c138b";
         private void Start()
         {
-            int adTypes = Appodeal.NON_SKIPPABLE_VIDEO;
-            Appodeal.initialize(AppKey, adTypes);
+            _deadCount = PlayerPrefs.GetInt("deadCount");
+            int adTypes = Appodeal.NON_SKIPPABLE_VIDEO | Appodeal.INTERSTITIAL;
+            Appodeal.initialize(APPKEY, adTypes, false);
         }
         private void OnEnable()
         {
-            OnReloadButtonClick.OnAction += ShowNonScipableVideo;
+            OnReloadButtonClick.OnAction += ShowAd;
+            OnDie.OnAction += DeadCountUp;
         }
         private void OnDisable()
         {
-            OnReloadButtonClick.OnAction -= ShowNonScipableVideo;
-        }
-        public void ShowNonScipableVideo()
-        {
-            if (Appodeal.canShow(Appodeal.NON_SKIPPABLE_VIDEO) && !Appodeal.isPrecache(Appodeal.NON_SKIPPABLE_VIDEO))
-                Appodeal.show(Appodeal.NON_SKIPPABLE_VIDEO);
+            OnReloadButtonClick.OnAction -= ShowAd;
+            OnDie.OnAction -= DeadCountUp;
         }
 
+        private void ShowAd()
+        {
+            if(PlayerPrefs.GetInt("deadCount") >= _deathToAd)
+            {
+                if (!TryShowConcretAd(Appodeal.INTERSTITIAL))
+                    TryShowConcretAd(Appodeal.NON_SKIPPABLE_VIDEO);
+                Debug.Log(PlayerPrefs.GetInt("deadCount"));
+            }
+        }
+
+        private bool TryShowConcretAd(int typeAd)
+        {
+            if (Appodeal.canShow(typeAd) && !Appodeal.isPrecache(typeAd))
+            {
+                Appodeal.show(typeAd);
+                _deadCount = 0;
+                PlayerPrefs.SetInt("deadCount", _deadCount);
+                return true;
+            }
+            return false;
+        }
+        private void DeadCountUp()
+        {
+            _deadCount += 1;
+            PlayerPrefs.SetInt("deadCount", _deadCount);
+        }
     }
 }
